d4721b0706d5abef6f42410ba6286fc485325cd0
Nishant Sharma <96777814+nishantsharma13903@users.noreply.github.com>
2025-08-05 10:40:04 +0530

    done

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,22 @@
 //   plugins: [react()],
 // })
 
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+// import { defineConfig } from 'vite'
+// import tailwindcss from '@tailwindcss/vite'
+// export default defineConfig({
+//   plugins: [
+//     tailwindcss(),
+//   ],
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: '../dist', 
+    emptyOutDir: true
+  }
+});
